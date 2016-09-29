b05352fa4a4878feea7881dc30099906ba626a8e
den1den <den_1_den@hotmail.com>
2016-09-29 13:35:00 +0200

    First attempt at creating similarity image

--- similarity.py
+++ similarity.py
@@ -9,6 +9,19 @@ def similarity_strings(string1, string2):
     return 0
 
 
+def sp_permute(A, perm_r, perm_c):
+    """
+    permute rows and columns of A
+    From https://github.com/JonnyRed/IPython-Notebooks/blob/e96686b94ade97750008536d1335c9b2ef032676/Numerical-Python/code-listing-py/ch10-code-listing.py
+    """
+    M, N = A.shape
+    # row permumation matrix
+    Pr = sp.coo_matrix((np.ones(M), (perm_r, np.arange(N)))).tocsr()
+    # column permutation matrix
+    Pc = sp.coo_matrix((np.ones(M), (np.arange(M), perm_c))).tocsr()
+    return Pr.T * A * Pc.T
+
+
 def similarity_tf(*strings):
     tfidf = TfidfVectorizer().fit_transform(strings)
     # no need to normalize, since Vectorizer will return normalized tf-idf
--- test_similarity.py
+++ test_similarity.py
@@ -1,10 +1,17 @@
 import json
 from unittest import TestCase
 
+import numpy
+from PIL.Image import fromarray
+from scipy.sparse.csgraph._reordering import reverse_cuthill_mckee
+
 from similarity import similarity_strings, similarity_tf
 
 with open("test_news/tc1.json") as fp:
     data = json.load(fp)
+
+with open("test_tweets/100-elections.json") as fp:
+    data2 = json.load(fp)
 
 
 class GeneralTestCase(TestCase):
@@ -16,10 +23,34 @@ class GeneralTestCase(TestCase):
 
 class TestCase1(TestCase):
     def test_similarity_news(self):
+        titles = [article['title'] for article in data]
+        bodies = [article['body'] for article in data]
         assert similarity_strings(data[0]['title'], data[1]['title']) == 0
-        pws = similarity_tf(*[article['title'] for article in data]).toarray()
-        assert pws[0][1] > pws[0][2]  # Article 1 is more similar to 2 then to 3
-        assert pws[0][1] > pws[1][2]  # Article 2 is more similar to 1 then to 3
-        assert pws[0][2] <= 0.1  # Article 3 is not very similar to 1
-        assert pws[1][2] <= 0.1  # Article 3 is not very similar to 2
-        assert pws[0][1] >= 0.1  # Article 1 and 2 are quite similar
+        title_sa = similarity_tf(*titles).toarray()
+        assert title_sa[0][1] > title_sa[0][2]  # Article 1 is more similar to 2 then to 3
+        assert title_sa[0][1] > title_sa[1][2]  # Article 2 is more similar to 1 then to 3
+        assert title_sa[0][2] <= 0.1  # Article 3 is not very similar to 1
+        assert title_sa[1][2] <= 0.1  # Article 3 is not very similar to 2
+        assert title_sa[0][1] >= 0.1  # Article 1 and 2 are quite similar
+
+        body_sa = similarity_tf(*bodies).toarray()
+        assert body_sa[0][1] > body_sa[0][2]  # Article 1 is more similar to 2 then to 3
+        assert body_sa[0][1] > body_sa[1][2]  # Article 2 is more similar to 1 then to 3
+
+        body_title_sa = similarity_tf(*(titles + bodies)).toarray()
+        # Title article X is more similar to body article X then bodies of diff articles
+        assert body_title_sa[0][3] > body_title_sa[0][4] and body_title_sa[0][3] > body_title_sa[0][5]
+        assert body_title_sa[2][5] > body_title_sa[2][3] and body_title_sa[2][5] > body_title_sa[2][4]
+        # Except for article 2, that is more similar to body of article 1 then body of article 2
+
+
+class TestCaseTweets(TestCase):
+    def test_100_tweets(self):
+        texts = [tweet['text'] for tweet in data2]
+        tweets_sim = similarity_tf(*texts)
+        minbandwidth_perm = reverse_cuthill_mckee(tweets_sim, True)
+        # Could use more efficient matricx prepresenation
+        tweets_sim_cuthillmckee = [[tweets_sim[x][y] for y in minbandwidth_perm] for x in minbandwidth_perm]
+        rgbss = numpy.array([numpy.array([numpy.array(el, 0, 0) for el in row]) for row in tweets_sim_cuthillmckee])
+        img = fromarray(rgbss, 'RGB')
+        img.save('my.png')
