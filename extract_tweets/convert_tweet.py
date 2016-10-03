2f71ace7bab82c6f8e8f1764eaecd520e1743a60
Tim Beishuizen <TimBeishuizen@hotmail.com>
2016-10-03 15:45:49 +0200

    Added method for tweet conversion to python structure

--- extract_tweets/convert_tweet.py
+++ extract_tweets/convert_tweet.py
@@ -5,3 +5,7 @@ from argparse import Namespace
 def convert_tweet(filename):
     with open(filename, encoding='utf8') as data_file:
         return json.load(data_file, object_hook=lambda d: Namespace(**d))
+
+tweets = convert_tweet('xaa.valid.json')
+
+[print("<>" + str(tweets[i]) + "</>") or print() for i in range(0, 9)]
