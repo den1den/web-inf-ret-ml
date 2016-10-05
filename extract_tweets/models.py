2040946de4ffaf46a42e0e853c6760ab8cbb5975
den1den <den_1_den@hotmail.com>
2016-10-05 15:36:25 +0200

    Added Tweet model

--- extract_tweets/models.py
+++ extract_tweets/models.py
@@ -1,9 +1,21 @@
 class Tweet(dict):
     def __init__(self, iterable=None, **kwargs):
         super().__init__(iterable, **kwargs)
+        self.id = self['id']
 
     def __str__(self, *args, **kwargs):
        return self['text']
 
+    def __hash__(self, *args, **kwargs):
+        # Hashlookups can be fatser if: return self.id
+        return super().__hash__(*args, **kwargs)
+
+    def __eq__(self, *args, **kwargs):
+        # equals can be faster?
+        return super().__eq__(*args, **kwargs)
+
     def get_hashtags(self):
         return [h['text'] for h in self['entities']['hashtags']]
+
+    def get_retweet_id(self):
+        return self["retweeted_status"]['id'] or None
