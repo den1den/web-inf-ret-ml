4e6c425d6b76cbb7854db317ad584fc21edc97a4
Dennis van den Brand <den_1_den@hotmail.com>
2016-10-26 19:45:42 +0200

    Added support for static content

--- frontend/runserver.sh
+++ frontend/runserver.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt --upgrade
 cd frontend
 #export DJANGO_SETTINGS_MODULE=frontend.settings_server
 python manage.py collectstatic --noinput
-exec gunicorn --name web_inf_ret --workers 3 --bind unix:/home/webinfret/web-inf-ret-ml/frontend/gunicorn.sock frontend.wsgi:application
+exec gunicorn --name web_inf_ret --workers 1 --bind unix:/home/webinfret/web-inf-ret-ml/frontend/gunicorn.sock frontend.wsgi:application
