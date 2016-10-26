378fd015332b07f898361fa7b12b3cd38d4a3d7a
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2016-10-26 20:53:58 +0200

    Moved django frontend app to base package

--- frontend/runserver.sh
+++ frontend/runserver.sh
@@ -3,7 +3,6 @@ cd /home/webinfret
 . venv/bin/activate
 cd web-inf-ret-ml
 pip install -r requirements.txt --upgrade
-cd frontend
 #export DJANGO_SETTINGS_MODULE=frontend.settings_server
 python manage.py collectstatic --noinput
 exec gunicorn --name web_inf_ret --workers 1 --bind unix:/home/webinfret/web-inf-ret-ml/frontend/gunicorn.sock frontend.wsgi:application
