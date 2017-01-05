e0f0749729a09394fcc6e780722b415fc49890b4
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2017-01-05 11:05:53 +0100

    Added tweet count and tweet cluster API: GET /api/tweet_count_week?start=2016%2050&end=2016%2051

--- frontend/api/serializer.py
+++ frontend/api/serializer.py
@@ -1,9 +1,34 @@
 from rest_framework import serializers
 
-from frontend.api.models import TweetCountCache, TweetCluster, Tweet
+from frontend.api.models import TweetCountCache, TweetCluster, Tweet, Article
 
 
 class TweetCountCacheSerializer(serializers.ModelSerializer):
     class Meta:
         model = TweetCountCache
         fields = ('count', 'day',)
+
+
+class TweetSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField(method_name='get_tweet_id')
+
+    class Meta:
+        model = Tweet
+        fields = ()
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField(method_name='get_article_id')
+
+    class Meta:
+        model = Article
+        fields = ()
+
+
+class TweetClusterSerializer(serializers.ModelSerializer):
+    tweets = TweetSerializer(many=True)
+    article = ArticleSerializer()
+
+    class Meta:
+        model = TweetCluster
+        fields = ('tweets', 'article', )
--- frontend/api/urls.py
+++ frontend/api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
-from frontend.api.views import TweetCountCacheViewSet, get_tweet_counts_week
+from frontend.api.views import TweetCountCacheViewSet, get_tweet_counts_week, TweetClusterViewSet
 
 router = DefaultRouter()
 router.register(r'tweet_count_cache', TweetCountCacheViewSet)
+router.register(r'tweet_cluster', TweetClusterViewSet)
 urlpatterns = router.urls + [
     url(r'tweet_count_week', get_tweet_counts_week)
 ]
