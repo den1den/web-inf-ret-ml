c33470a3f8adf08a57b7f4de4c7450c79a897287
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2017-01-13 21:36:47 +0100

    added cors headers

--- frontend/base/settings.py
+++ frontend/base/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'frontend.api',
     'frontend.home',
@@ -45,12 +46,19 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'group1.ga',
+    'localhost',
+    '127.0.0.1'
+)
 
 ROOT_URLCONF = 'frontend.base.urls'
 
