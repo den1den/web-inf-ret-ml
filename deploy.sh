2ecf23d5cd7d243afa987c02feef161b418b4f94
Dennis van den Brand <den_1_den@hotmail.com>
2017-01-13 12:56:51 +0100

    Fixed deployment

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
-echo "Updating web-inf-ret-ml"
+echo "Updating: web-inf-ret-ml"
 cd /home/webinfret/web-inf-ret-ml
 git status
 git pull
-echo "Updating web-inf-retrieval"
+echo ""
+echo "Updating: web-inf-retrieval"
 cd /home/webinfret/web-inf-retrieval
 git status
 git pull
-
-supervisorctl restart webinfret
+echo ""
+echo "Updating: webinfret"
+cd /home/webinfret/webinfret
+git status
+git pull
+echo ""
+echo "Updating: IndexBuilder"
+cd /home/webinfret/IndexBuilder
+git status
+git pull
+cp src/main/java/indexing/Config.java.server.txt src/main/java/indexing/Config.java
+./restart-glashfish.sh
