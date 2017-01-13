bbadb9a68f9b60f7d231ce2a8dc38004969a5487
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2017-01-13 15:19:50 +0100

    added load_tweetcounts commands

---
+++ frontend/api/management/commands/load_tweetcounts.py
@@ -0,0 +1,24 @@
+import datetime
+import json
+import os
+
+from django.core.management.base import BaseCommand, CommandError
+
+from frontend.api.models import TweetCluster, Article, Tweet, TweetCountCache
+
+
+class Command(BaseCommand):
+    help = 'Load all tweet counts'
+
+    def handle(self, *args, **options):
+        start = datetime.datetime(2016, 1, 1)
+        end = datetime.datetime(2017, 1, 1)
+
+        n = 0
+        d = start
+        while d < end:
+            tc = TweetCountCache.get_or_create(day=d)
+            n += tc.count
+            d += datetime.timedelta(days=1)
+
+        self.stdout.write(self.style.SUCCESS('Successfully imported clusters, created %d entries' % n))
