49b112125cd4e5027905b5474cb886ab1c0e8fef
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2017-01-13 16:54:39 +0100

    parse date correctly of article

--- inputoutput/string_fixer.py
+++ inputoutput/string_fixer.py
@@ -9,6 +9,7 @@ BOOLEAN = set()
 BOOLEAN.add('ends_dots')
 DATE_SIMPLE = set()
 DATE_SIMPLE.add('published_date')
+DATETIME_SIMPLE = set()
 
 ARRAY.add('mentions'), ARRAY.add('hashtags'), ARRAY.add('keywords'), ARRAY.add('media'), ARRAY.add('quotations'), ARRAY.add('symbols'), ARRAY.add('urls'), ARRAY.add('currencies'),
 INT.add('keyword_length'), INT.add('n_abbriviations'), INT.add('n_html_entities'), INT.add('n_punctuations'), INT.add('n_quationmarks'), INT.add('n_unicode'), INT.add('newline_count')
@@ -27,7 +28,9 @@ def fix_types(d):
             d[key] = int(value)
         elif key in BOOLEAN:  # Check if supposed to be a boolean
             d[key] = bool(value)
-        elif key in DATE_SIMPLE:
+        elif key in DATETIME_SIMPLE:
             d[key] = datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+        elif key in DATE_SIMPLE:
+            d[key] = datetime.strptime(value, '%Y-%m-%d')
         else:  # Just string
             d[key] = value
